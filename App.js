4ee4c99052b4c3693bf328af3829e40e0d625fe9
matthewmjm <matthewmjm@yahoo.com>
2021-01-01 15:38:54 -0700

    setup welcome screen

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import WelcomeScreen from './src/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <WelcomeScreen />
+  )
 }
 
 const styles = StyleSheet.create({
