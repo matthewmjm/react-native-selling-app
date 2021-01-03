22db0ab4913884bc9556cc560de92925014e7b09
matthewmjm <matthewmjm@yahoo.com>
2021-01-02 22:12:50 -0700

    implement pull to refresh

--- src/screens/MessagesScreen.js
+++ src/screens/MessagesScreen.js
@@ -23,6 +23,7 @@ const initialMessages = [
 const MessagesScreen = (props) => {
 
     const [messages, setMessages] = useState(initialMessages);
+    const [refreshing, setRefreshing] = useState(false);
 
     const handleDelete = message => {
         // Delete message from messages array
@@ -48,6 +49,17 @@ const MessagesScreen = (props) => {
                     /> 
                 )}
                 ItemSeparatorComponent={ListItemSeparator}
+                refreshing={refreshing}
+                onRefresh={() => {
+                    setMessages([
+                        {
+                            id: 2,
+                            title: 'T2',
+                            description: 'D2',
+                            image: require('../assets/mosh.jpg')
+                        },
+                    ])
+                }}
             />
         </Screen>
     );
