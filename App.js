0db481decd3b3cb488af44df33c2bc18c2ad8d14
matthewmjm <matthewmjm@yahoo.com>
2021-01-03 19:22:26 -0700

    finishing Text Input

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, SafeAreaView, TextInput } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Constants from 'expo-constants';
 import WelcomeScreen from './src/screens/WelcomeScreen';
@@ -14,17 +14,23 @@ import Icon from './src/components/Icon';
 import ListItem from './src/components/ListItem';
 import AccountScreen from './src/screens/AccountScreen';
 import ListingsScreen from './src/screens/ListingsScreen';
+import AppTextInput from './src/components/AppTextInput'
+
 
 const App = () => {
+  const [firstName, setFirstName] = useState('');
+
   return (
-    <ListingsScreen />
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
