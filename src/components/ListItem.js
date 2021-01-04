14fd1dbf7fe13833711a43a5208ab9ffdfa08230
matthewmjm <matthewmjm@yahoo.com>
2021-01-04 13:04:56 -0700

    add chevrons to messages screen

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Image, StyleSheet, TouchableHighlight } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 import AppText from './AppText';
 import colors from '../config/colors';
@@ -18,6 +19,7 @@ const ListItem = ({ title, subTitle, image, IconComponent, onPress, renderRightA
                         <AppText style={styles.title}>{title}</AppText>
                         { subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
                     </View>
+                    <MaterialCommunityIcons name="chevron-right" size={25} color={colors.medium}/>
                 </View>
             </TouchableHighlight>
         </Swipeable>
@@ -26,11 +28,13 @@ const ListItem = ({ title, subTitle, image, IconComponent, onPress, renderRightA
 
 const styles = StyleSheet.create({
     container: {
+        alignItems: 'center',
         flexDirection: 'row',
         padding: 15,
         backgroundColor: colors.white,
     },
     detailsContainer: {
+        flex: 1,
         marginLeft: 10,
         justifyContent: "center",
     },
