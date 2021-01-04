2d9132790985ef4e0301b8fd2db4dee6d478c6d1
matthewmjm <matthewmjm@yahoo.com>
2021-01-04 08:40:38 -0700

    create submit button component

---
+++ src/components/SubmitButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { useFormikContext } from 'formik';
+import AppButton from './AppButton';
+
+const SubmitButton = ({ title }) => {
+    const { handleSubmit } = useFormikContext();
+    return (
+        <>
+            <AppButton 
+                title={title}
+                onPress={handleSubmit} 
+            />
+        </>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default SubmitButton;
